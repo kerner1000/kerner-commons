e0f613e3d76359a6f7e1a53f4fff99974389ff97
2407 <2407@gmx.org>
2009-11-06 17:56:16 +0000

    Introduce collection package.

--- src/de/kerner/commons/collection/IterableMap.java
+++ src/de/kerner/commons/collection/IterableMap.java
@@ -1,14 +1,9 @@
 package de.kerner.commons.collection;
 
-import java.util.Iterator;
 import java.util.Map;
 
-public abstract class IterableMap<K, V> implements Map<K, V>, Iterable<K>{
+public interface IterableMap<K, V> extends Map<K, V>, Iterable<K>{
 	
-	// Implement //
-	
-	public Iterator<K> iterator() {
-		return keySet().iterator();
-	}
+
 
 }

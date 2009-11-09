16c997dae2d8023f616940f11dbbc5bf91250a8d
2407 <2407@gmx.org>
2009-11-09 13:09:13 +0000

    introduce SLF4J helper class.

---
+++ src/de/kerner/commons/logging/Log.java
@@ -0,0 +1,52 @@
+package de.kerner.commons.logging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ * <p> slf4j-logging helper class </p>
+ * @lastVisit 2009-10-05
+ * @author Alexander Kerner
+ *
+ */
+public class Log {
+	
+	//TODO: move to kerner.commons
+
+	private final Logger logger;
+
+	public Log(Logger logger) {
+		this.logger = logger;
+	}
+
+	public Log(Class<?> c) {
+		this.logger = LoggerFactory.getLogger(c);
+	}
+
+	public void debug(Object msg) {
+		if (logger.isDebugEnabled()) {
+			logger.debug(msg.toString());
+		}
+	}
+
+	public void debug(Object msg, Throwable t) {
+		if (logger.isDebugEnabled()) {
+			logger.debug(msg.toString(), t);
+		}
+	}
+
+	public void info(String msg) {
+		if (logger.isInfoEnabled()) {
+			logger.info(msg);
+		}
+	}
+
+	public void warn(String msg) {
+		logger.warn(msg);
+	}
+	
+	public void warn(String msg, Throwable t) {
+		logger.warn(msg, t);
+	}
+}

de234c5cc03c1b7d90314f01d0f4bed70642011e
2407 <2407@gmx.org>
2009-11-09 11:53:23 +0000

    small fix.

--- src/de/kerner/commons/StringUtils.java
+++ src/de/kerner/commons/StringUtils.java
@@ -4,7 +4,7 @@ public class StringUtils {
 
 	public static String getString(Object... objects) {
 		if (objects == null || objects.length == 0)
-			return null;
+			return "";
 		final StringBuilder sb = new StringBuilder(objects.length);
 		for (Object o : objects) {
 			sb.append(o);

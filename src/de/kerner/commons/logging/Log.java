23a8bb3a259cdefe7f410e9dc3b0cc7c85ad3c44
2407 <2407@gmx.org>
2009-11-10 10:42:01 +0000

    Add void error(Object msg) and void(Object msg, Throwable t).

--- src/de/kerner/commons/logging/Log.java
+++ src/de/kerner/commons/logging/Log.java
@@ -43,10 +43,23 @@ public class Log {
 	}
 
 	public void warn(String msg) {
+		if(logger.isWarnEnabled())
 		logger.warn(msg);
 	}
 	
 	public void warn(String msg, Throwable t) {
+		if(logger.isWarnEnabled())
 		logger.warn(msg, t);
 	}
+	
+	public void error(String msg) {
+		if(logger.isErrorEnabled())
+		logger.error(msg);
+	}
+	
+	public void error(String msg, Throwable t) {
+		if(logger.isErrorEnabled())
+		logger.error(msg, t);
+	}
 }
+

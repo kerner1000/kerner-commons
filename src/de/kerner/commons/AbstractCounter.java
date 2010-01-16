d6fd28fb9499a6eec37dd137c0784579ffced755
2407 <2407@gmx.org>
2010-01-16 19:24:46 +0000

    * API-BREAK * Add javadoc for "AbstractCounter". Also few changes for this class.

--- src/de/kerner/commons/AbstractCounter.java
+++ src/de/kerner/commons/AbstractCounter.java
@@ -1,25 +1,37 @@
+/**********************************************************************
+Copyright (c) 2010 Alexander Kerner. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ ***********************************************************************/
+
 package de.kerner.commons;
 
-//TODO: move to kerner commons
+/**
+ * <p>
+ * description // TODO
+ * </p>
+ * 
+ * @author Alexander Kerner
+ * @threadSave primitive class members volatile
+ * @lastVisit 2010-01-16
+ * 
+ */
 public abstract class AbstractCounter {
 
 	private volatile int interval = 0;
 	private volatile int intervalHelper = 0;
 	private volatile int cnt = 0;
 
-	public void count() {
-		cnt++;
-		intervalHelper++;
-		checkPerform();
-	}
-	
-	public void stop() {
-		perform();
-	}
-
-	public void setPrintInterval(int interval) {
-		this.interval = interval;
-	}
+	// Private //
 
 	private void checkPerform() {
 		if (intervalHelper >= interval) {
@@ -28,12 +40,63 @@ public abstract class AbstractCounter {
 		}
 	}
 
-	protected int getCnt(){
+	// Public //
+
+	/**
+	 * <p>
+	 * Increases counter by 1.
+	 * </p>
+	 */
+	public void count() {
+		cnt++;
+		intervalHelper++;
+		checkPerform();
+	}
+
+	/**
+	 * <p>
+	 * Resets this {@code AbstractCounter} and calls
+	 * {@link AbstractCounter#perform()}.
+	 * </p>
+	 */
+	public void stop() {
+		interval = 0;
+		intervalHelper = 0;
+		cnt = 0;
+		perform();
+	}
+
+	/**
+	 * <p>
+	 * Sets {@code interval} for this {@code AbstractCounter}.
+	 * </p>
+	 * 
+	 * @param interval
+	 *            number of counts {@link AbstractCounter#count()} has to be
+	 *            called before {@link AbstractCounter#perform()} is called.
+	 */
+	public void setInterval(int interval) {
+		this.interval = interval;
+	}
+
+	/**
+	 * <p>
+	 * Returns the number of {@link AbstractCounter#count()} has been called.
+	 * </p>
+	 * 
+	 * @return current counts
+	 */
+	public int getCnt() {
 		return cnt;
 	}
-	
-	protected abstract void perform();
 
-	
+	// Abstract //
+
+	/**
+	 * <p>
+	 * Performs action.
+	 * </p>
+	 */
+	public abstract void perform();
 
 }

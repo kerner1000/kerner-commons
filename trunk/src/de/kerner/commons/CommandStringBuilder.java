bb60a2babeb6e366cbbb946a9105410e0a816503
2407@gmx.org <2407@gmx.org@b93d95c8-3027-11de-8e0c-53a27eea117e>
2010-01-20 22:45:46 +0000

    Stuff

--- trunk/src/de/kerner/commons/CommandStringBuilder.java
+++ trunk/src/de/kerner/commons/CommandStringBuilder.java
@@ -1,3 +1,18 @@
+/**********************************************************************
+Copyright (c) 2010 Alexander Kerner. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ ***********************************************************************/
+
 package de.kerner.commons;
 
 import java.util.ArrayList;
@@ -11,42 +26,75 @@ import java.util.Map.Entry;
  * Helper Class to build a Command line argument string, which can easily passed
  * to constructor of {@link java.lang.ProcessBuilder}
  * </p>
+ * <p>
+ * Example: TODO
+ * </p>
  * 
  * @author Alexander Kerner
- * @lastVisit 2009-10-01
+ * @see ProcessBuilder
+ * @threadSave
  */
 public class CommandStringBuilder {
 
 	private final List<String> sb = new ArrayList<String>();
 
+	// Constructor //
+
 	/**
+	 * <p>
+	 * Constructor TODO
+	 * </p>
 	 * 
-	 * @param executable absolute or relative path to executable binary.
+	 * @param executable
+	 *            absolute or relative path to executable binary.
 	 */
 	public CommandStringBuilder(String executable) {
 		sb.add(executable);
 	}
-	
+
+	/**
+	 * <p>
+	 * Constructor TODO
+	 * </p>
+	 * 
+	 * @param template
+	 *            {@code template} on which this {@code CommandStringBuilder}
+	 *            is initially created.
+	 */
 	public CommandStringBuilder(List<String> template) {
-		for(String s : template){
-			sb.add(s);
-		}
-	}
-	
-	public CommandStringBuilder(CommandStringBuilder template) {
-		for(String s : template.getCommandList()){
+		for (String s : template) {
 			sb.add(s);
 		}
 	}
 
 	/**
-	 * <p> Add a command line argument that follows pattern
-	 * <blockquote> <code>identifier</code> <code>value</code></blockquote>
-	 * for instance
+	 * <p>
+	 * Constructor TODO
+	 * </p>
+	 * 
+	 * @param template
+	 *            {@code template} on which this {@code CommandStringBuilder}
+	 *            is initially created.
+	 */
+	public CommandStringBuilder(CommandStringBuilder template) {
+		for (String s : template.getCommandList()) {
+			sb.add(s);
+		}
+	}
+	
+	// Public //
+
+	/**
+	 * <p>
+	 * Add a command line argument that follows pattern <blockquote>
+	 * <code>identifier</code> <code>value</code></blockquote> for instance
 	 * <blockquote> <code>--verbose</code> <code>true</code></blockquote>
 	 * </p>
-	 * @param identifier name of the command line argument
-	 * @param value value if the command line argument
+	 * 
+	 * @param identifier
+	 *            name of the command line argument
+	 * @param value
+	 *            value if the command line argument
 	 */
 	public CommandStringBuilder addValueCommand(String identifier, String value) {
 		sb.add(identifier);
@@ -55,12 +103,15 @@ public class CommandStringBuilder {
 	}
 
 	/**
-	 * <p> Add all command line arguments from a collection, which follow pattern
-	 * <blockquote> <code>identifier</code> <code>value</code></blockquote>
-	 * for instance
-	 * <blockquote> <code>--verbose</code> <code>true</code></blockquote>
+	 * <p>
+	 * Add all command line arguments from a collection, which follow pattern
+	 * <blockquote> <code>identifier</code> <code>value</code></blockquote> for
+	 * instance <blockquote> <code>--verbose</code> <code>true</code>
+	 * </blockquote>
 	 * </p>
-	 * @param all Collection of command line arguments
+	 * 
+	 * @param all
+	 *            Collection of command line arguments
 	 */
 	public CommandStringBuilder addAllValueCommands(Map<String, String> all) {
 		for (Entry<String, String> e : all.entrySet()) {
@@ -70,34 +121,50 @@ public class CommandStringBuilder {
 		return this;
 	}
 
+	/**
+	 * <p> TODO </p>
+	 * @param all TODO
+	 */
 	public CommandStringBuilder addAllFlagCommands(String... all) {
 		sb.addAll(Arrays.asList(all));
 		return this;
 	}
 
+	/**
+	 * <p> TODO </p>
+	 * @param all TODO
+	 */
 	public CommandStringBuilder addAllFlagCommands(List<String> all) {
 		sb.addAll(all);
 		return this;
 	}
 
 	/**
-	 * <p> Add a command line argument, that does not expect any value. <br>
-	 * Pattern:
-	 * <blockquote> <code>identifier</code></blockquote>
-	 * for instance
+	 * <p>
+	 * Add a command line argument that does not expect any value. </p><p>
+	 * Pattern: <blockquote> <code>identifier</code></blockquote> for instance
 	 * <blockquote> <code>--help</code></blockquote>
 	 * </p>
-	 * @param flagCommand command line argument
+	 * 
+	 * @param flagCommand
+	 *            command line argument
 	 */
 	public CommandStringBuilder addFlagCommand(String flagCommand) {
 		sb.add(flagCommand);
 		return this;
 	}
 
+	/**
+	 * TODO
+	 * @return TODO
+	 */
 	public List<String> getCommandList() {
 		return sb;
 	}
+	
+	// Override //
 
+	@Override
 	public String toString() {
 		return sb.toString();
 	}

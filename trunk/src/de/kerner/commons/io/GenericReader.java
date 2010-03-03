0cf12aa345cef99ba59aa57045364f469012c6cd
2407@gmx.org <2407@gmx.org@b93d95c8-3027-11de-8e0c-53a27eea117e>
2010-03-03 12:33:32 +0000

    Introduce GenericWriter and GenericReader

---
+++ trunk/src/de/kerner/commons/io/GenericReader.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package de.kerner.commons.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+
+/**
+ * <p>
+ * An implementation of {@code GenericReader} can be read from disk/file, from a
+ * {@code Reader} or from an {@code InputStream}.
+ * </p>
+ * 
+ * @author Alexander Kerner
+ * @see File
+ * @see Reader
+ * @see InputStream
+ * 
+ */
+public interface GenericReader {
+	
+	public long read(File file) throws IOException;
+	
+	public void read(Reader reader) throws IOException;
+	
+	public void read(InputStream stream) throws IOException; 
+
+}

e9d6f816e3596598ce10fbcab1eba8c4b77778d6
2407@gmx.org <2407@gmx.org@b93d95c8-3027-11de-8e0c-53a27eea117e>
2010-03-03 13:06:14 +0000

    Introduce BufferedStringReader and BufferedStringWriter.

---
+++ trunk/src/de/kerner/commons/file/BufferedStringReader.java
@@ -0,0 +1,48 @@
+package de.kerner.commons.file;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import de.kerner.commons.io.AbstractGenericReader;
+
+/**
+ * <p>
+ * Read a string-based file line by line.
+ * </p>
+ * <p>
+ * A line is considered to be terminated by any one of a line feed ('\n'), a
+ * carriage return ('\r'), or a carriage return followed immediately by a
+ * linefeed.
+ * </p>
+ * <p>
+ * {@code BufferedStringReader} will use a {@link BufferedReader} internally for buffered reading.
+ * </p>
+ * 
+ * @author Alexander Kerner
+ * @see BufferedReader
+ * 
+ */
+public abstract class BufferedStringReader extends AbstractGenericReader {
+
+	// Implement //
+
+	public void read(Reader reader) throws IOException {
+		if (reader == null)
+			throw new NullPointerException();
+		BufferedReader br = new BufferedReader(reader);
+		try {
+			String strLine;
+			while ((strLine = br.readLine()) != null) {
+				handleLine(strLine);
+			}
+		} finally {
+			// cannot happen
+			// if(stream != null)
+			reader.close();
+		}
+	}
+
+	public abstract void handleLine(String line);
+
+}
